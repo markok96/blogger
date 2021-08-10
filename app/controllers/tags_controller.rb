c2e19e1a2544d49707a55ae13d48b52f8a498995
Marko Korolija <mkorolija3@gmail.com>
2021-08-10 17:57:10 +0200

    added authentication

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,19 @@
+class TagsController < ApplicationController
+
+	before_action :require_login, only: [:destroy]
+
+	def show
+		@tag = Tag.find(params[:id])
+	end
+
+	def index
+		@tags = Tag.all
+	end
+
+	def destroy
+		@tag = Tag.find(params[:id])
+		@tag.destroy
+
+		redirect_back fallback_location: "/tags"
+	end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -73,4 +73,20 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  Paperclip.options[:command_path] = 'C:\Program Files (x86)\GnuWin32\bin; C:\Program Files\ImageMagick-6.9.12-Q8'
+  Paperclip.options[:swallow_stderr] = false
+
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action.mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'example.com',
+    user_name:            '<username>',
+    password:             '<password>',
+    authentication:       'plain',
+    enable_starttls_auto: true
+    }
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
 end
