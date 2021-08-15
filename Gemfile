547dce51dc18b64f4e5f72bc915cfb1cd08d4138
Marko Korolija <mkorolija3@gmail.com>
2021-08-15 22:06:58 +0200

    test for archive_article

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ gem 'jbuilder', '~> 2.7'
 # gem 'bcrypt', '~> 3.1.7'
 gem "paperclip", "~> 6.0.0"
 gem 'sorcery' 
-
+gem 'rexml', '~> 3.2', '>= 3.2.5'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- test/mailers/archive_mailer_test.rb
+++ test/mailers/archive_mailer_test.rb
@@ -1,12 +1,19 @@
 require "test_helper"
 
 class ArchiveMailerTest < ActionMailer::TestCase
+  def setup
+    @article = Article.last
+  end
+
   test "article_archived" do
-    mail = ArchiveMailer.article_archived
-    assert_equal "Article archived", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = ArchiveMailer.article_archived(@article)
+    assert_equal "Article #{@title} has been archived", mail.subject
+    assert_equal [@email], mail.to
+    assert_equal ["jonathanpeterson314@gmail.com"], mail.from
+    assert_match "Hello <%= @user %>
+
+
+Your article <%= @title %> has been archived.", mail.body.encoded
   end
 
 end
